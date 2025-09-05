eff74627d706816c032fce3bda1326d724acaf9d
Navid M <navidmomtahen@gmail.com>
2025-09-05 01:03:40 +0100

    amend

--- verse.go
+++ verse.go
@@ -1,68 +1,20 @@
 package main
 
 import (
-	"database/sql"
 	"log"
+	"verse/database"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/mustache/v2"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// Represents some user of the system
-type User struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-// The singleton database instance
-//
-// TODO: This will be changed to a connection pool
-var db *sql.DB
-
-func initDatabase() error {
-	var err error
-	db, err = sql.Open("sqlite3", "./users.db")
-	if err != nil {
-		return err
-	}
-	return createTable()
-}
-
-func createTable() error {
-	query := `CREATE TABLE IF NOT EXISTS users (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		email TEXT UNIQUE NOT NULL,
-		password TEXT NOT NULL
-	)`
-	_, err := db.Exec(query)
-	return err
-}
-
-func createUser(email, password string) error {
-	query := `INSERT INTO users (email, password) VALUES (?, ?)`
-	_, err := db.Exec(query, email, password)
-	return err
-}
-
-func getUserByEmail(email string) (*User, error) {
-	query := `SELECT id, email, password FROM users WHERE email = ?`
-	row := db.QueryRow(query, email)
-
-	user := &User{}
-	err := row.Scan(&user.ID, &user.Email, &user.Password)
-	if err != nil {
-		return nil, err
-	}
-	return user, nil
-}
-
 func main() {
-	if err := initDatabase(); err != nil {
+	if err := database.InitDatabase(); err != nil {
 		log.Fatal("Failed to initialize database:", err)
 	}
-	defer db.Close()
+
+	defer database.CloseConnection()
 
 	var (
 		engine = mustache.New("./views", ".mustache")
@@ -88,8 +40,7 @@ func main() {
 		if email == "" || password == "" {
 			return c.Status(400).SendString("Email and password are required")
 		}
-
-		if err := createUser(email, password); err != nil {
+		if err := database.CreateUser(email, password); err != nil {
 			return c.Status(500).SendString("Failed to create user")
 		}
 
@@ -99,16 +50,13 @@ func main() {
 	app.Post("/signin", func(c *fiber.Ctx) error {
 		email := c.FormValue("email")
 		password := c.FormValue("password")
-
 		if email == "" || password == "" {
 			return c.Status(400).SendString("Email and password are required")
 		}
-
-		user, err := getUserByEmail(email)
+		user, err := database.GetUserByEmail(email)
 		if err != nil || user.Password != password {
 			return c.Status(401).SendString("Invalid credentials")
 		}
-
 		return c.Render("index", fiber.Map{
 			"Email": user.Email,
 		})
