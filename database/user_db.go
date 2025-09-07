913c2583137bc24343da0f48e4c2a446d96d28f9
Navid M <navidmomtahen@gmail.com>
2025-09-07 01:41:04 +0100

    newline

--- database/user_db.go
+++ database/user_db.go
@@ -33,6 +33,7 @@ func UpdateUser(userID int, email, username, password string) error {
 }
 
 // Adds a feed item to user's reading list
+//
 // Returns (saved or not -> bool, error)
 func SaveToReadingList(userID int, itemID string) (bool, error) {
 	exists, err := IsInReadingList(userID, itemID)
