0c202ddf978e3175634aa17be591567c35574201
Navid M <navidmomtahen@gmail.com>
2025-09-07 12:39:51 +0100

    restructure

---
+++ static/js/sign-up.js
@@ -0,0 +1,16 @@
+const darkModeToggle = document.getElementById("darkModeToggle");
+const toggleSlider = document.getElementById("toggleSlider");
+const html = document.documentElement;
+
+html.classList.add("dark");
+
+darkModeToggle.addEventListener("click", () => {
+   html.classList.toggle("dark");
+   if (html.classList.contains("dark")) {
+      toggleSlider.classList.remove("translate-x-6");
+      toggleSlider.classList.add("translate-x-1");
+   } else {
+      toggleSlider.classList.remove("translate-x-1");
+      toggleSlider.classList.add("translate-x-6");
+   }
+});
---
+++ static/js/tw-config-alt.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+   darkMode: "class",
+   theme: {
+      extend: {
+         colors: {
+            brand: "#1f2937",
+            "brand-light": "#374151",
+         },
+      },
+   },
+};
