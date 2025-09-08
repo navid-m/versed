f584c8ce764fe01d0b1b0567a8c24e7d54fa2abb
Navid M <navidmomtahen@gmail.com>
2025-09-08 16:11:17 +0100

    remove duplicate

--- static/js/subverse.js
+++ static/js/subverse.js
@@ -164,11 +164,6 @@ class SubverseManager {
                         <span class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-red-100 dark:bg-red-900 text-red-800 dark:text-red-200">
                            ${post.post_type}
                         </span>
-                        <button class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-600 transition-colors"
-                           onclick="viewPostComments(${post.id})">
-                           <i class="far fa-comments mr-1"></i>
-                           View comments
-                        </button>
                      </div>
                   </div>
                </div>
